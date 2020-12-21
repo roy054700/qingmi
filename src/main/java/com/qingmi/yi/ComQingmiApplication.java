1d0937aa786254d61fbcae1f961005c6936a7d60
Your Name <you@example.com>
2020-12-21 12:14:44 +0800

    第3次提交

--- src/main/java/com/qingmi/yi/ComQingmiApplication.java
+++ src/main/java/com/qingmi/yi/ComQingmiApplication.java
@@ -3,6 +3,9 @@ package com.qingmi.yi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ */
 @SpringBootApplication
 public class ComQingmiApplication {
 
